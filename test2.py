bb1cc1c5928cc4ad3eb116fc6b9251ec76927787
Nadine Randel <nadine@voodoo-arts.net>
2022-01-28 20:37:51 +0000

    fetching neurons

--- test2.py
+++ test2.py
@@ -16,3 +16,23 @@ us_2hop_neurons = pymaid.get_partners(us_neurons.skeleton_id, directions=['incom
 
 # check names
 pymaid.get_names(neurons)
+
+# get treenode table for skid from previously loaded neurons 
+tn_table = pymaid.get_node_table(neurons[0])
+tn_table.head()
+
+# fetching neurons
+n_list1 = pymaid.get_neuron(['annotation:eyespot']) #by annotation
+
+n_list2 = pymaid.get_neuron(['772993', '57341']) #by name or skid
+
+
+# %%
+# finding downstream partners (with no threshold)
+ds_neurons = pymaid.get_partners(neurons, directions=['outgoing']) # could add threshold here
+ds_2hop_neurons = pymaid.get_partners(ds_neurons.skeleton_id, directions=['outgoing'])
+
+
+
+# %%
+
