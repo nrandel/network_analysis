d7df1d04b23eb5f1484049985e31e21f2b698d74
Nadine Randel <nadine@voodoo-arts.net>
2023-03-27 11:39:39 +0100

    upstream

--- multihop_example.py
+++ multihop_example.py
@@ -11,11 +11,21 @@ rm = pymaid.CatmaidInstance(url, token, name, password)
 from contools import Promat
 
 # %%
-neurons = pymaid.get_skids_by_annotation('nr Tel-like 10') # Tel-like 10
+neurons = pymaid.get_skids_by_annotation('nr neuron 65408') # Tel-like 10
 
 # use pregenerated edge list
 edges = pd.read_csv('data/edges_threshold/ad_pairwise-input-threshold-0.01_paired-edges_2022-11-03.csv', index_col=0)
 
 # downstream 3-hops of Tel-like 10
-downstream = Promat.downstream_multihop(edges=edges, sources=neurons, hops=1, pairs_combined=True)
+downstream = Promat.downstream_multihop(edges=edges, sources=neurons, hops=3, pairs_combined=True)
+
+
+# %%
+neurons = pymaid.get_skids_by_annotation('nr Tel-like 10') # Tel-like 10
+
+# use pregenerated edge list
+edges = pd.read_csv('data/edges_threshold/ad_pairwise-input-threshold-0.01_paired-edges_2022-11-03.csv', index_col=0)
+
+# upstream 3-hops of Tel-like 10
+upstream = Promat.upstream_multihop(edges=edges, sources=neurons, hops=3, pairs_combined=True)
 # %%
