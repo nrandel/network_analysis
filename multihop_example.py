39a76999dbc3e9aa6651e36534586f84fa32f921
Nadine Randel <nadine@voodoo-arts.net>
2023-03-28 17:06:18 +0100

    update with paired and unpaired list

--- multihop_example.py
+++ multihop_example.py
@@ -9,23 +9,34 @@ import pymaid
 rm = pymaid.CatmaidInstance(url, token, name, password)
 
 from contools import Promat
+from data_settings import data_date_A1_brain, pairs_path
+
+pairs = Promat.get_pairs(pairs_path=pairs_path) #csv of neuron pairs
+#pairs = Promat.get_pairs(pairs_path=pairs_path, flip_weirdos=False) 
+# usually this function flips the left and right IDs of neurons that have their soma in one
+# brain hemisphere and their neurites in the order. You can deactivate this behaviour with flip_weirdos=False
 
 # %%
-neurons = pymaid.get_skids_by_annotation('nr neuron 65408') # Tel-like 10
+# when looking at multihop connectivity with a threshold using individual neuron edge list (not the paired list); TESTED AND WORKING
 
-# use pregenerated edge list
-edges = pd.read_csv('data/edges_threshold/ad_pairwise-input-threshold-0.01_paired-edges_2022-11-03.csv', index_col=0)
-
-# downstream 3-hops of Tel-like 10
-downstream = Promat.downstream_multihop(edges=edges, sources=neurons, hops=3, pairs_combined=True)
-
-
-# %%
 neurons = pymaid.get_skids_by_annotation('nr Tel-like 10') # Tel-like 10
 
 # use pregenerated edge list
-edges = pd.read_csv('data/edges_threshold/ad_pairwise-input-threshold-0.01_paired-edges_2022-11-03.csv', index_col=0)
+edges = Promat.pull_edges(type_edges='ad', threshold=0.01, data_date=data_date_A1_brain, pairs_combined=False)
+pairs = Promat.get_pairs(pairs_path=pairs_path) #csv of neuron pairs
 
-# upstream 3-hops of Tel-like 10
-upstream = Promat.upstream_multihop(edges=edges, sources=neurons, hops=3, pairs_combined=True)
+# downstream 3-hops of Tel-like 10
+downstream = Promat.downstream_multihop(edges=edges, sources=neurons, hops=2, pairs_combined=False, pairs=pairs)
+
+# %%
+# when looking at multihop connectivity with a threshold using paired-neuron edge list; TESTED AND WORKING 
+# (not individual neurons; note that pair_ID refers to the left skid of a pair)
+
+neurons = pymaid.get_skids_by_annotation('nr Tel-like 10') # Tel-like 10
+
+# use pregenerated edge list
+edges = Promat.pull_edges(type_edges='ad', threshold=0.01, data_date=data_date_A1_brain, pairs_combined=True)
+
+# downstream 3-hops of Tel-like 10
+downstream = Promat.downstream_multihop(edges=edges, sources=neurons, hops=2, pairs_combined=True)
 # %%
